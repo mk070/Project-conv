35076c10dc44a1379d8e172544d512c0ddb62717
Sukanth07 <sukanthkannansk@gmail.com>
2024-09-03 15:29:52 +0530

    Refactor backend structure validation code

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,10 +7,13 @@ from .additionals.analyzer import analyze_mern
 class ProjectUploadView(APIView):
     def post(self, request, *args, **kwargs):
         print('\n-----------------------------------------------------------------------\n')
-        github_link = request.data.get('github_link', None)
         media_path = 'media'
         folder_structure = None
+        is_valid = False
+        error = None
         
+        github_link = request.data.get('github_link', None)
+
         if github_link:
             repo_path = utils.handle_github_link(media_path, github_link)
             folder_structure = utils.parse_project_structure(repo_path)
@@ -27,8 +30,8 @@ class ProjectUploadView(APIView):
             is_valid, error = analyze_mern.validate_backend_structure(folder_structure)
             print('\n-----------------------------------------------------------------------\n')
 
-            if is_valid:
-                return Response({'message': 'Project structure is valid.',
-                                 'folderStructure': folder_structure}, status=status.HTTP_200_OK)
-            else:
-                return Response({'error': error}, status=status.HTTP_400_BAD_REQUEST)
+        if is_valid:
+            return Response({'message': 'Project structure is valid.',
+                                'folderStructure': folder_structure}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': error}, status=status.HTTP_400_BAD_REQUEST)
