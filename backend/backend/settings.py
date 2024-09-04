eb5b3a4ade5a5ac567e0df9b485746d50f06b310
Madhan <madhan.king51@gmail.com>
2024-09-04 09:23:24 +0530

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Allow requests from Vite frontend
+    "http://localhost:5174",  # Allow requests from Vite frontend
 ]
 
 # Media files (uploads)
